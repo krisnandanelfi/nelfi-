aa66b274df7cdcdbc80f5af726c0538ab0559061
krisnandanelfi <nelfi0205@gmail.com>
2022-05-13 10:20:42 +0700

    first commit

---
+++ app/Controllers/AyoController.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class AyoController extends Controller{
+    public function index()
+    {
+        return view('AyoView');
+    }
+
+}
